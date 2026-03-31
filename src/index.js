a3626953c2fca63a7d7923529238bfdb4c3da229
Arslane101 <rahalabdelhamid@gmail.com>
2026-03-31 18:02:08 +0100

    whatever

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-
+import "./style.css";
 const weatherapi = process.env.VISUAL_CROSSING 
+const gihpy = process.env.GIPHY
+
+const search = document.getElementById("search")
 ///fetch weather data 
 async function GetWeather(location) {
     const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+location+`?key=${weatherapi}` 
@@ -7,10 +10,17 @@ async function GetWeather(location) {
         const response = await fetch(url)
         if(!response.ok) throw new Error(`Response status: ${response.status}`)
         const result = await response.json()
-        console.log(result)
+        return result
     } catch(error){
         console.log(error.message)
     }
 }
 
-GetWeather("london")
+/*search.addEventListener("keypress",() => {
+    let result = GetWeather(search.value)
+
+})
+*/
+/// result.currentConditions.conditions , result.currentConditions.temp , .humidity , .feelslike
+let results = GetWeather("london")
+console.log(results)
